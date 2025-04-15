873736f10f5b4ef22c6d28b44bb797dfcc467018
Saket Wakekar <saketwakekar@gmail.com>
2025-04-15 17:37:40 +0530

    Enabled Feign Client

--- OrderService/src/main/java/com/microservices/OrderService/Service/OrderServiceImpl.java
+++ OrderService/src/main/java/com/microservices/OrderService/Service/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package com.microservices.OrderService.Service;
 
 import com.microservices.OrderService.Entity.Order;
+import com.microservices.OrderService.External.Clients.ProductService;
 import com.microservices.OrderService.Model.OrderRequest;
 import com.microservices.OrderService.Repositiory.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +15,16 @@ public class OrderServiceImpl implements OrderService {
 
     @Autowired
     private OrderRepository orderRepository;
+
+    @Autowired
+    private ProductService productService;
     private static final Logger logger = Logger.getLogger(OrderServiceImpl.class.getName());
 
     @Override
     public long placeOrder(OrderRequest orderRequest) {
+
+        productService.reduceQuantity(orderRequest.getProductId(),orderRequest.getQuantity());
+
         logger.info("Placing Order Request " + orderRequest);
         Order order = new Order(orderRequest.getProductId(), orderRequest.getQuantity(), Instant.now(),"CREATED",orderRequest.getAmount());
         orderRepository.save(order);
